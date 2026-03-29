50fe62c8bd1d4f9e57dc231451158055a2fb41bc
Abdullah <abdullahhaleem530@gmail.com>
2026-03-29 15:28:11 +0500

    first commit

---
+++ 02_reading.mongodb.js
@@ -0,0 +1,8 @@
+use ('ecommerce');
+
+// db.products.find({"name": "Wireless Mouse"})
+// db.products.find({category: "Electronics"})
+// db.products.find({ price: { $gt: 1000 } }) 
+// db.products.find({ price: { $gte: 1000, $lte: 5000 } })
+// db.products.find({}, { name: 1, price: 1, _id: 0 })
+db.products.find().sort({ price: -1 }).skip(1).limit(1)
---
+++ 03_update.mongodb.js
@@ -0,0 +1,15 @@
+use('ecommerce')
+// Update the price of the "Wireless Mouse" to 899
+// db.products.updateOne(
+//   { name: "Wireless Mouse" },
+//   { $set: { price: 899 } }
+// )
+
+// db.products.updateMany(
+//     { category: "Electronics" },
+//     { $inc: { stock: 11 } }
+// )
+db.products.updateOne(
+    {name: "Wireless Mouse"},
+    { $push: { tags:"Mouse" } }
+)
